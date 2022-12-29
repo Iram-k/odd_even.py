a7ad3b944a2fd7a6452cf5d6149b3cd9f2593ce5
Iram-k <114799889+Iram-k@users.noreply.github.com>
2022-12-29 16:58:37 +0530

    Create odd_even.py

---
+++ odd_even.py
@@ -0,0 +1,13 @@
+ans='y'
+while ans=='y':
+    num=int(input("Enter a Number :"))
+    if num % 2==0:
+        print(num,"is EVEN number.")   
+    else:
+         print(num,"is ODD number.")
+        
+    ans=input('''want more entry more number ?
+(y/n)..''') 
+
+    
+    
